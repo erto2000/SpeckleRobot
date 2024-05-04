84fa8ab67025f40b731900275892d9e4aacbfc34
MrShaTr <56830053+MrShaTr@users.noreply.github.com>
2024-05-04 13:29:12 +0300

    Added different models for comparison + Augmentation code

---
+++ augment.py
@@ -0,0 +1,121 @@
+
+import PIL
+import torch
+from PIL import Image
+from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import torchvision.transforms as T
+import os
+
+# torch.transforms
+
+
+
+# Gausian Blur
+gausian_blur_transformation_13 = T.GaussianBlur(kernel_size=(7, 13), sigma=(6, 9))
+gausian_blur_transformation_56 = T.GaussianBlur(kernel_size=(7, 13), sigma=(5, 8))
+
+
+# Gausian Noise
+
+def addnoise(input_image, noise_factor=0.3):
+    inputs = T.ToTensor()(input_image)
+    noisy = inputs + torch.rand_like(inputs) * noise_factor
+    noisy = torch.clip(noisy, 0, 1.)
+    output_image = T.ToPILImage()
+    image = output_image(noisy)
+    return image
+
+
+# Colour Jitter
+
+colour_jitter_transformation_1 = T.ColorJitter(brightness=(0.5, 1.5), contrast=(3), saturation=(0.3, 1.5),
+                                               hue=(-0.1, 0.1))
+
+colour_jitter_transformation_2 = T.ColorJitter(brightness=(0.7), contrast=(6), saturation=(0.9), hue=(-0.1, 0.1))
+
+colour_jitter_transformation_3 = T.ColorJitter(brightness=(0.5, 1.5), contrast=(2), saturation=(1.4), hue=(-0.1, 0.5))
+
+
+
+# Main function that calls all the above functions to create 11 augmented images from one image
+
+def augment_image(img_path):
+    # orig_image
+    orig_img = Image.open(Path(img_path))
+
+    # Gausian Blur
+
+    gausian_blurred_image_13_image = gausian_blur_transformation_13(orig_img)
+    # gausian_blurred_image_13_image.show()
+
+    gausian_blurred_image_56_image = gausian_blur_transformation_56(orig_img)
+    # gausian_blurred_image_56_image.show()
+
+    # Gausian Noise
+
+    gausian_image_3 = addnoise(orig_img)
+
+    # gausian_image_3.show()
+
+    gausian_image_6 = addnoise(orig_img, 0.6)
+
+    # gausian_image_6.show()
+
+    gausian_image_9 = addnoise(orig_img, 0.9)
+
+    # gausian_image_9.show()
+
+    # Color Jitter
+
+    colour_jitter_image_1 = colour_jitter_transformation_1(orig_img)
+
+    # colour_jitter_image_1.show()
+
+    colour_jitter_image_2 = colour_jitter_transformation_2(orig_img)
+
+    # colour_jitter_image_2.show()
+
+    colour_jitter_image_3 = colour_jitter_transformation_3(orig_img)
+
+    # colour_jitter_image_3.show()
+
+    return [orig_img,
+            gausian_blurred_image_13_image, gausian_blurred_image_56_image, gausian_image_3, gausian_image_6,
+            gausian_image_9, colour_jitter_image_1, colour_jitter_image_2, colour_jitter_image_3]
+
+
+# augmented_images = augment_image(orig_img_path)
+
+def creating_file_with_augmented_images(file_path_master_dataset, file_path_augmented_images):
+    master_dataset_folder = file_path_master_dataset
+    files_in_master_dataset = os.listdir(file_path_master_dataset)
+    augmented_images_folder = file_path_augmented_images
+
+    counter = 0
+
+    for element in files_in_master_dataset:
+        os.mkdir(f"{augmented_images_folder}/{element}")
+        images_in_folder = os.listdir(f"{master_dataset_folder}/{element}")
+        counter = counter + 1
+        counter2 = 0
+        for image in images_in_folder:
+            required_images = augment_image(f"{master_dataset_folder}/{element}/{image}")
+            counter2 = counter2 + 1
+            counter3 = 0
+            for augmented_image in required_images:
+                counter3 = counter3 + 1
+                augmented_image = augmented_image.save(
+                    f"{augmented_images_folder}/{element}/{counter}_{counter2}_{counter3}_{image}")
+
+# augmented dataset path
+augmented_dataset = "/Users/mahirdemir/Desktop/pyhon_vs/git_interact/SpeckleRobot/SpeckleRobotDatasetAug"
+
+# master dataset path
+master_dataset = "/Users/mahirdemir/Desktop/pyhon_vs/git_interact/SpeckleRobot/SpeckleRobotDataset"
+
+# run the program
+
+creating_file_with_augmented_images(master_dataset, augmented_dataset)
--- torchModel.py
+++ torchModel.py
@@ -15,24 +15,31 @@ import seaborn as sns
 
 
 # Define constants
-dataset_path = '/Users/mahirdemir/Desktop/pyhon_vs/git_interact/SpeckleRobot/SpeckleRobotDataset'
+dataset_path = '/Users/mahirdemir/Desktop/pyhon_vs/git_interact/SpeckleRobot/SpeckleRobotDatasetAug'
 img_height, img_width = 224, 224
 num_classes = 3
-batch_size = 16
+batch_size = 64
 num_epochs = 5
 save_path = 'TrainedModels\model.pth'
+
+use_resnet_50 = True
+use_resnet_18 = False
+use_moblilenet_v3 = False
+use_sqeezenet = False
 
 # Define data preprocessing and augmentation transforms
 data_transforms = {
     'train': transforms.Compose([
-        transforms.RandomResizedCrop(img_height),
-        transforms.RandomHorizontalFlip(),
+        transforms.Resize((img_height, img_width)),
+        #transforms.RandomResizedCrop(img_height),
+        #transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ]),
     'val': transforms.Compose([
         transforms.Resize((img_height, img_width)),
-        transforms.CenterCrop(img_height),
+        #transforms.CenterCrop(img_height),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ]),
@@ -84,17 +91,36 @@ train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
 # Load pre-trained ResNet model
-model = models.resnet50(pretrained=True)
-num_ftrs = model.fc.in_features
-model.fc = nn.Linear(num_ftrs, num_classes)
+if use_resnet_50:
+    model = models.resnet50(pretrained=True)
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, num_classes)
+elif use_resnet_18:
+    model = models.resnet18(pretrained=True)
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, num_classes)
+elif use_moblilenet_v3:
+    model = models.mobilenet_v3_large(pretrained=True)
+    num_ftrs = model.classifier[-1].in_features
+    model.classifier[-1] = nn.Linear(num_ftrs, num_classes)
+elif use_sqeezenet:
+    model = models.squeezenet1_0(pretrained=True)
+    model.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1, 1), stride=(1, 1))
+    model.num_classes = num_classes
 
 # Freeze convolutional layers
 for param in model.parameters():
     param.requires_grad = False
 
-# Unfreeze parameters of the fully connected layer
-for param in model.fc.parameters():
-    param.requires_grad = True
+if use_resnet_50 or use_resnet_18:
+    for param in model.fc.parameters():
+        param.requires_grad = True
+elif use_moblilenet_v3:
+    for param in model.classifier[-1].parameters():
+        param.requires_grad = True
+elif use_sqeezenet:
+    for param in model.classifier.parameters():
+        param.requires_grad = True
 
 # Define loss function and optimizer
 criterion = nn.CrossEntropyLoss()
@@ -105,6 +131,12 @@ device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 model = model.to(device)
 
 # Training loop
+
+train_losses = []
+train_accuracies = []
+total_t = 0
+correct_t = 0
+
 for epoch in tqdm(range(num_epochs)):
     model.train()
     running_loss = 0.0
@@ -116,7 +148,13 @@ for epoch in tqdm(range(num_epochs)):
         loss.backward()
         optimizer.step()
         running_loss += loss.item() * inputs.size(0)
+        _, predicted_t = torch.max(outputs, 1)
+        total_t += labels.size(0)
+        correct_t += (predicted_t == labels).sum().item()
     epoch_loss = running_loss / len(train_dataset)
+    epoch_accuracy = correct_t / total_t
+    train_losses.append(epoch_loss)
+    train_accuracies.append(epoch_accuracy)
 
     # Validation loop
     model.eval()
@@ -147,7 +185,28 @@ print(conf_matrix)
 print('Training finished.')
 
 
+
 torch.save(model, save_path)
+
+plt.figure(figsize=(12, 6))
+
+plt.subplot(1, 2, 1)
+plt.plot(train_losses, label='Training Loss', color='blue')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.title('Training Loss')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(train_accuracies, label='Training Accuracy', color='green')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.title('Training Accuracy')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+
 # Plot confusion matrix as heatmap
 plt.figure(figsize=(10, 8))
 sns.heatmap(conf_matrix, annot=True, fmt="d", cmap="Blues", cbar=False)
